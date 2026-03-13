4ab9e57d263f655c0bd719f8086d1efc40f4c132
Carlos <abejorrodigital@gmail.com>
2026-03-13 00:16:22 -0400

    Update Nosotros.tsx

--- src/pages/Nosotros.tsx
+++ src/pages/Nosotros.tsx
@@ -45,7 +45,7 @@ export default function Nosotros() {
           <div className="flex flex-col md:flex-row items-center gap-12">
             <div className="w-full md:w-1/2">
               <img
-                src="https://raw.githubusercontent.com/AbejorroDigital/la-iglesia-de-la-ciudad-demo/main/src/data/assets/pastor-josé-sanchez.JPG"
+                src="https://i.postimg.cc/NFkvybzv/pastor-jose-sanchez.jpg"
                 alt="Retrato profesional y cálido del Apóstol José Sánchez, transmitiendo liderazgo y empatía"
                 className="rounded-3xl shadow-lg object-cover w-full h-[500px]"
                 referrerPolicy="no-referrer"
